494617cf14a4dbb4d7101d00ec1848bb1f4fd827
RaphaelHarrison <raphaelsantos229@gmail.com>
2026-04-02 20:50:42 -0300

    Subindo Arquivos feitos na terça-feira(31/03)

---
+++ aula02/atividade01.py
@@ -0,0 +1,12 @@
+# numero = int(input("Digite um número para receber o sucessor e o antecessor: "))
+# sucessor = numero + 1
+# antecessor = numero - 1
+
+# print(f"Seu número é {numero}, o antecessor é {antecessor} e o sucessor é {sucessor}")
+
+#--------------------------------------------------------------------
+salario = float(input("Informe seu salário: "))
+aumento = salario * 0.15
+salario_aumento = salario + aumento
+
+print(f"Seu salário atual é de {salario:.2f} e com o aumento seu novo salário será de {salario_aumento:.2f}")
---
+++ aula02/exemplo01.py
@@ -0,0 +1,42 @@
+# # #nome = "Maria"
+# # #idade = 21
+
+# # #print(f"Olá {nome}\nVocê tem {idade}")
+
+# # #----------------------------------------------
+# # # nome = "Carlos"
+# # # profissao = "Médico"
+# # # bairro = "Barreto"
+
+# # # print(f"Nome: {nome}\nProfissão: {profissao}\nBairro: {bairro}")
+
+# # #-----------------------------------------------------------------------
+# Operadores
+
+# preco = 20
+# quantidade = 3
+# total = preco * quantidade
+# print(f"O total é {total}")
+# ---------------------------------------------------------------
+# salario = 3000
+# bonus = 880
+# Salario_total = salario + bonus
+# print(f"Seu salário esse mês será de {Salario_total}")
+#-------------------------------------------------------------------
+
+# nome = input("Qual é o seu nome: ")
+# print(f"Olá {nome}")
+#--------------------------------------------------------------------------------
+# salario = float(input("Qua é o seu salário: "))
+# bonus = float(input("Valor do bonus: "))
+# total = salario + bonus
+# print(f"O seu salário esse mês será de {total}")
+
+#----------------------------------------------------------------------------
+# Cálculo de Porcentagem:
+# preco = float(input("Valor do produto: "))
+# quantidade = int(input("Quantidade: "))
+# total = preco * quantidade
+# desconto = total * 0.1
+# total_final = total - desconto
+# print(f"O total da compra é {total}, mas com o desconto o valor final é {total}")
